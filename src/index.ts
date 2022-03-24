a89f4d7e5371192bc2176ae5c1d2b2cb2ebd6e4e
Francesco Orazini <horace.supertramp@gmail.com>
2022-03-24 19:20:15 +0100

    Add basic structure

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,83 @@ import util from 'util';
 import fs from 'fs/promises';
 import child_process from 'child_process';
 import moment from 'moment';
+import path from 'path';
+
+interface Student {
+    githubId: string;
+}
+
+type Exercise = string;
+
+interface CopyTestOptions {
+    copiedStudent: Student;
+    copyingStudent: Student;
+    exercise: Exercise;
+}
+
+interface DownloadExerciseOptions {
+    student: Student;
+    exercise: Exercise;
+    folder: string;
+}
+
+async function isGitRepo(folder: string) {
+    try {
+        fs.access(path.resolve(folder, '.git'));
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+async function updateExercise({
+    student,
+    exercise,
+    folder,
+}: DownloadExerciseOptions) {
+    await fs.mkdir(folder, { recursive: true });
+    const command = `git -C ${folder} clone https://github.com/${student.githubId}/${exercise}.git`;
+    await util.promisify(child_process.exec)(command);
+}
+
+/***
+ * Confronts two different students' deliveries of the same exercise.
+ */
+async function copyTest({
+    copiedStudent,
+    copyingStudent,
+    exercise,
+}: CopyTestOptions) {
+    const timestamp = moment().format('DD-MM-YYYY_HH-MM-SS');
+    const baseTestFolder = path.resolve('data', 'copyTests', `${copyingStudent.githubId}-${copiedStudent.githubId}-${timestamp}`);
+    const clonesFolder = path.resolve(baseTestFolder, 'clones');
+    const copyingStudentFolder = path.resolve(clonesFolder, copyingStudent.githubId);
+    const copiedStudentFolder = path.resolve(clonesFolder, copiedStudent.githubId);
+    await fs.mkdir(copyingStudentFolder, { recursive: true });
+    await fs.mkdir(copiedStudentFolder, { recursive: true });
+    await updateExercise({
+        exercise,
+        student: copyingStudent,
+        folder: copyingStudentFolder,
+    });
+    await updateExercise({
+        exercise,
+        student: copiedStudent,
+        folder: copiedStudentFolder,
+    });
+}
 
 (async() => {
     try {
-        const exec = util.promisify(child_process.exec);
-        const studentGithubId = process.argv[2];
-        const exerciseName = process.argv[3];
-        const id = `${studentGithubId}-${exerciseName}-${moment().format('DD-MM-YYYY')}`;
-        const folder = `./data/clones/${id}`;
-        console.log(`Cloning https://github.com/${studentGithubId}/${exerciseName}.git into ${folder}`);
-        await fs.mkdir(folder, { recursive: true });
-        await exec(`git -C ${folder} clone https://github.com/${studentGithubId}/${exerciseName}.git`);
+        await copyTest({
+            copyingStudent: {
+                githubId: process.argv[2],
+            },
+            copiedStudent: {
+                githubId: process.argv[3],
+            },
+            exercise: process.argv[4]
+        });
     } catch(error) {
         console.error(error);
         process.exit(1);
