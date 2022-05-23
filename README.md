299dbf41be666152ceee63f469dff904b075ef19
Francesco Orazini <horace.supertramp@gmail.com>
2022-05-23 20:05:49 +0200

    Clears names, fix info

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
-# Delivery copy detection tool
+# Boolean Utils
 
-Compare students file and find simple similarities.
+A small set of everyday life utilities to interact with certain types of Github hosted public projects
 
-## Usage
+# Install
+
+npm install -g https://github.com/HoraceSupertramp/utils.git
+
+# Usage
+
+```repo-utils [github_options] [command]```
+
+where
+
+## [github_options]
+
+> ```-h, --help``` Display usage<br>
+```-u, --user``` Github username<br>
+```-r, --repo``` Repo name (must be a public<br>repo)
+
+## [command]
+
+### ```start [command_options]```
+
+>Runs some commands in some user's repo. Commands can be specified manually (through the --run flag) or as a preset (--vue and --laravel flags).
+>
+>#### [command_options]
+>
+>>```--run``` Runs some commands after the repo has been cloned. Multiple commands must be specified with ```&&```<br>
+>>```--laravel``` Runs, in sequence:<br>
+>> - ```composer install```
+>> - ```php artisan key:generate``` (if needed)
+>> - ```php artisan serve```
+
+# Examples
+
+Clones this same repo and tries to run it as a laravel project (don't do this, it does not work).
+
+```repo-utils -u HoraceSupertramp -r repo-utils start --laravel```
 
--- src/index.ts
+++ src/index.ts
@@ -22,53 +22,53 @@ async function pathExists(path: string) {
     }
 }
 
-function getStudentPath(student: string, config: Config) {
-    return path.resolve(config.data, 'students', student);
+function getUsersPath(user: string, config: Config) {
+    return path.resolve(config.data, 'users', user);
 }
 
-function getDeliveriesPath(student: string, config: Config) {
-    return path.resolve(getStudentPath(student, config), 'deliveries');
+function getReposPath(user: string, config: Config) {
+    return path.resolve(getUsersPath(user, config), 'deliveries');
 }
 
-function getDeliveryPath(student: string, delivery: string, config: Config) {
-    return path.resolve(getDeliveriesPath(student, config), delivery);
+function getRepoPath(user: string, name: string, config: Config) {
+    return path.resolve(getReposPath(user, config), name);
 }
 
-async function cloneDelivery(student: string, delivery: string, config: Config) {
-    const folder = getDeliveriesPath(student, config);
-    console.log(`Cloning ${delivery} ${student}`.green);
+async function cloneRepo(user: string, repo: string, config: Config) {
+    const folder = getReposPath(user, config);
+    console.log(`Cloning ${repo} ${user}`.green);
     await fs.mkdir(folder, { recursive: true });
-    const command = `git -C ${folder} clone https://github.com/${student}/${delivery}.git`;
+    const command = `git -C ${folder} clone https://github.com/${user}/${repo}.git`;
     await exec(command);
 }
 
-async function updateDelivery(student: string, delivery: string, config: Config) {
-    const folder = getDeliveryPath(student, delivery, config);
-    console.log(`Pulling ${delivery} ${student}`.green);
+async function updateRepo(user: string, repo: string, config: Config) {
+    const folder = getRepoPath(user, repo, config);
+    console.log(`Pulling ${repo} ${user}`.green);
     const command = `git -C ${folder} pull`;
     await exec(command);
 }
 
-async function inspectDelivery(student: string, delivery: string, config: Config) {
-    const deliveryPath = getDeliveryPath(student, delivery, config);
+async function inspectRepo(user: string, repo: string, config: Config) {
+    const repoPath = getRepoPath(user, repo, config);
     return {
-        exists: await pathExists(deliveryPath),
-        isGitRepo: await pathExists(path.resolve(deliveryPath, '.git')),
+        exists: await pathExists(repoPath),
+        isGitRepo: await pathExists(path.resolve(repoPath, '.git')),
     };
 }
 
-async function syncronizeDelivery(student: string, delivery: string, config: Config) {
-    const deliveryPath = path.resolve(config.data, 'students', student, delivery);
-    const deliveryFolder = await inspectDelivery(student, delivery, config);
-    if (deliveryFolder.exists) {
-        if (deliveryFolder.isGitRepo) {
-            await updateDelivery(student, delivery, config);
+async function syncronizeRepo(user: string, repo: string, config: Config) {
+    const repoPath = path.resolve(config.data, 'users', user, repo);
+    const repoFolder = await inspectRepo(user, repo, config);
+    if (repoFolder.exists) {
+        if (repoFolder.isGitRepo) {
+            await updateRepo(user, repo, config);
         } else {
-            await fs.rm(deliveryPath, { recursive: true, force: true });
-            await cloneDelivery(student, delivery, config);
+            await fs.rm(repoPath, { recursive: true, force: true });
+            await cloneRepo(user, repo, config);
         }
     } else {
-        await cloneDelivery(student, delivery, config);
+        await cloneRepo(user, repo, config);
     }
 }
 
@@ -89,7 +89,7 @@ async function runCommand(command: string, folder: string, pipe: boolean) {
 }
 
 interface GlobalOptions {
-    student: string;
+    user: string;
     repo: string;
     npm: boolean;
     data?: string;
@@ -105,10 +105,10 @@ interface RunOptions {
     try {
         const program = new Command();
         program.requiredOption('-r, --repo <repo>');
-        program.requiredOption('-s, --student <github_id>');
+        program.requiredOption('-u, --user <github_id>');
         program.option('-d, --data <path>');
         program.command('start')
-            .description('clone one student\'s delivery and run a command within it')
+            .description('clone one user\'s repo and starts it')
             .option('--laravel')
             .option('--vue')
             .option('-r, --run <commands>')
@@ -119,49 +119,49 @@ interface RunOptions {
             }: RunOptions) => {
                 try {
                     const {
-                        student,
-                        repo,
+                        user,
+                        repo: name,
                         data = path.resolve(__dirname, '..', 'data'),
                     } = program.opts() as GlobalOptions;
                     const config: Config = { data };
-                    const delivery = getDeliveryPath(student, repo, config);
-                    await syncronizeDelivery(student, repo, config);
+                    const repo = getRepoPath(user, name, config);
+                    await syncronizeRepo(user, repo, config);
                     const commands = userCommands 
                         ? userCommands
                             .split('&&')
                             .map((command) => command.trim())
                         : [];
                     if (laravel) {
-                        const dotenvExample = path.resolve(delivery, '.env.example');
-                        const dotenv = path.resolve(delivery, '.env');
+                        const dotenvExample = path.resolve(repo, '.env.example');
+                        const dotenv = path.resolve(repo, '.env');
                         if (await pathExists(dotenvExample)) {
                             await fs.copyFile(dotenvExample, dotenv);
                         }
                         await runCommand(
                             'composer install',
-                            delivery,
+                            repo,
                             true
                         );
-                        for (const command of commands) {
-                            await runCommand(
-                                command,
-                                delivery,
-                                true
-                            );
-                        }
                         if (await pathExists(dotenv)) {
                             const contents = envfile.parse(await fs.readFile(dotenv, 'utf-8'));
                             if (contents['APP_KEY'] == null || contents['APP_KEY'] === '') {
                                 await runCommand(
                                     'php artisan key:generate',
-                                    delivery,
+                                    repo,
                                     true
                                 );
                             }
                         }
+                        for (const command of commands) {
+                            await runCommand(
+                                command,
+                                repo,
+                                true
+                            );
+                        }
                         await runCommand(
                             'php artisan serve',
-                            delivery,
+                            repo,
                             true
                         );
                     }
